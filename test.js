e8abbe322bf26d1b37e09162e50d834481866cf5
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-12-02 21:49:27 +0900

    add test to check function names

--- test.js
+++ test.js
@@ -5,25 +5,34 @@ var test = require('tape');
 
 function runTest(description, main) {
   test(description, function(t) {
-    t.plan(4);
+    t.plan(5);
+
+    t.equal(
+      main.name,
+      'isSpdxLicenseIdentifier', 'should have a function name.'
+    );
 
     t.strictEqual(
-      main('MIT'), true,
+      main('MIT'),
+      true,
       'should return true when the string is a SPDX license identifier.'
     );
 
     t.strictEqual(
-      main('foo'), false,
+      main('foo'),
+      false,
       'should return false when the string is not a SPDX license identifier.'
     );
 
     t.strictEqual(
-      main(true), false,
+      main(true),
+      false,
       'should return false when the argument is not a string.'
     );
 
     t.strictEqual(
-      main(), false,
+      main(),
+      false,
       'should return false when it takes no arguments.'
     );
   });
