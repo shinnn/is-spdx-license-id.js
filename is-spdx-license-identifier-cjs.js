005f4886a9677f545718aa114abaa1773da12772
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-12-02 21:51:23 +0900

    move 'use strict' to the function body

--- is-spdx-license-identifier-cjs.js
+++ is-spdx-license-identifier-cjs.js
@@ -2,10 +2,8 @@
  * is-spdx-license-identifier | MIT (c) Shinnosuke Watanabe
  * https://github.com/shinnn/is-spdx-license-identifier.js
 */
-'use strict';
-
 var spdxLicenseIdentifiers = require('spdx-license-identifiers');
-
 module.exports = function isSpdxLicenseIdentifier(str) {
+  'use strict';
   return spdxLicenseIdentifiers.indexOf(str) !== -1;
 };
--- is-spdx-license-identifier.js
+++ is-spdx-license-identifier.js
@@ -2,8 +2,7 @@
  * is-spdx-license-identifier | MIT (c) Shinnosuke Watanabe
  * https://github.com/shinnn/is-spdx-license-identifier.js
 */
-'use strict';
-
 window.isSpdxLicenseIdentifier = function isSpdxLicenseIdentifier(str) {
+  'use strict';
   return window.spdxLicenseIdentifiers.indexOf(str) !== -1;
 };
